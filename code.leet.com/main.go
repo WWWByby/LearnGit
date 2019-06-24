c8d2ad82d1b4749e515204f853eb581126cdd18a
snake <18501604724@163.com>
2019-06-24 18:14:10 +0800

    service

--- code.leet.com/main.go
+++ code.leet.com/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/micro/go-micro"
+	proto "github.com/micro/examples/greeter/srv/proto/hello"
+
+	"golang.org/x/net/context"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
+	rsp.Msg = "Hello " + req.Name
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("greeter"),
+		micro.Version("latest"),
+	)
+
+	service.Init()
+
+	proto.RegisterSayHandler(service.Server(), new(Greeter))
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
